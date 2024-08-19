a02ce56628aedfe60aeedb555522dd32414f91f7
imandreans <andrean.sihombing83@gmail.com>
2024-08-19 10:28:05 +0800

    refactor javascript code

--- script.js
+++ script.js
@@ -7,28 +7,8 @@ fetch("/data.json")
     return req.json();
   })
   .then((data) => {
-    let periods = new Map();
-    periods.set("daily", "day");
-    periods.set("weekly", "week");
-    periods.set("monthly", "month");
     data.forEach((data) => {
-      let title =
-        `${data.title}` == "Self Care"
-          ? "self-care"
-          : `${data.title}`.toLowerCase();
-
-      const activity = document.querySelector(
-        `#${title} .description .duration`
-      );
-      //---------------------------Short Ver V
-      periods.forEach((time, period) => {
-        document.getElementById(period).addEventListener("click", (e) => {
-          e.preventDefault();
-          const { current, previous } = data.timeframes[period];
-          activity.innerHTML = `<h1>${current}Hr</h1> <h2>Last ${time} - ${previous}Hrs</h2>`;
-        });
-      });
-
+      showTime(data);
       //---------------------------Long Ver V
       // document.getElementById("daily").addEventListener("click", (e) => {
       //   let current = data.timeframes.daily.current;
@@ -49,4 +29,24 @@ fetch("/data.json")
     });
   });
 
+const showTime = (data) => {
+  let periods = new Map();
+  periods.set("daily", "day");
+  periods.set("weekly", "week");
+  periods.set("monthly", "month");
+  let title =
+    `${data.title}` == "Self Care"
+      ? "self-care"
+      : `${data.title}`.toLowerCase();
+
+  const activity = document.querySelector(`#${title} .description .duration`);
+  //---------------------------Short Ver V
+  periods.forEach((time, period) => {
+    document.getElementById(period).addEventListener("click", (e) => {
+      e.preventDefault();
+      const { current, previous } = data.timeframes[period];
+      activity.innerHTML = `<h1>${current}Hr</h1> <h2>Last ${time} - ${previous}Hrs</h2>`;
+    });
+  });
+};
 // window.addEventListener('load', clickButton);
